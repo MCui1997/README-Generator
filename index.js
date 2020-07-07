4ffc67c0ed842dd70ce5aada0e1fd904c1a11b65
Michael Cui <cuimichael500@gmail.com>
2020-07-07 16:49:15 -0500

    Added inquirer and test prompts

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+var fileName = "michael.md";
+
+
 // array of questions for user
 const questions = [
 
 ];
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What languages do you know?",
+      name: "language"
+    },
+    {
+      type: "input",
+      message: "What is your preferred method of communication?",
+      name: "communication"
+    }
+  ])
+  .then(function(response) {
+      
+    writeToFile(fileName,response);
+
+  });
+
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(file, response) {
+
+
+    fs.writeFile(file,JSON.stringify(response,null, '\t'),function(err) {
+
+    });
 }
 
 // function to initialize program
@@ -12,5 +47,3 @@ function init() {
 
 }
 
-// function call to initialize program
-init();
